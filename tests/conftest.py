f48a41cb9a7dce07f8d5e2a77b147a8f3fc17559
vlandeau <vlandeau@ekinox.io>
2025-06-20 13:30:23 +0200

    chore: reorder config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,13 @@
 import pytest
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def vcr_config():
     return {
         "record_mode": "new_episodes",
         "match_on": [
-            "host",
-            "port",
-            "path",
-            "body",
+            "host", #  If you want a new recording when changing the LLM service (e.g., openai to gemini)
+            "body", #  To match the prompt
         ],
         "filter_headers": [("authorization", "DUMMY")],
     }
